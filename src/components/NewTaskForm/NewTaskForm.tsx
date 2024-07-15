74c57acbf7ff3980895ec9d5e361ce6d3d2da5fb
kenkenpa198 <kenkenpa198@gmail.com>
2024-07-15 19:16:32 +0900

    add create tasks action

--- src/components/NewTaskForm/NewTaskForm.tsx
+++ src/components/NewTaskForm/NewTaskForm.tsx
@@ -1,7 +1,48 @@
+'use client';
+
+import { createTask, FormState } from '@/actions/task';
+import { useFormState, useFormStatus } from 'react-dom';
+
 const NewTaskForm = () => {
+  // FormState の初期状態
+  const initialState: FormState = { error: '' };
+
+  // Server Actions の結果と Server Actions と同じ機能を持つ formAction 関数を取得する
+  // state                   : Server Actions の戻り値
+  // formAction              : Server Actions と同じ動きをする関数
+  // useFormState の第一引数 : Server Actions 本体 (createTask)
+  // useFormState の第二引数 : state の初期値 (initialState)
+  const [state, formAction] = useFormState(createTask, initialState);
+
+  /**
+   * SubmitButton
+   *
+   * - useFormStatus の戻り値を取得し、送信処理中は非活性 (disabled) にしてボタンを押せなくする
+   *
+   * @returns - Button の JSX
+   */
+  const SubmitButton = () => {
+    // Server Actions の実行状態を取得する
+    const { pending } = useFormStatus();
+
+    return (
+      <button
+        type="submit"
+        className="mt-8 w-full rounded-md bg-gray-800 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 disabled:bg-gray-400"
+        disabled={pending}
+      >
+        Create
+      </button>
+    );
+  };
+
   return (
     <div className="mx-auto mt-10 w-full max-w-sm">
-      <form action="">
+      {/*
+        form の action 属性へ formAction 関数を渡す
+        フォームの内容が Server Actions へ送信され、タスクが作成される
+      */}
+      <form action={formAction}>
         <div>
           <label htmlFor="title" className="block text-sm font-medium">
             タイトル
@@ -40,12 +81,10 @@ const NewTaskForm = () => {
             className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
           />
         </div>
-        <button
-          type="submit"
-          className="mt-8 w-full rounded-md bg-gray-800 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700"
-        >
-          Create
-        </button>
+        <SubmitButton />
+        {state.error && (
+          <p className="mt-2 text-sm text-red-500">{state.error}</p>
+        )}
       </form>
     </div>
   );
