518aa4f66fe06b362c2816b14a557a0aab2cefb2
kenkenpa198 <kenkenpa198@gmail.com>
2024-07-15 20:08:46 +0900

    タスク編集機能の実装 Part1

--- src/actions/task.ts
+++ src/actions/task.ts
@@ -39,3 +39,36 @@ export const createTask = async (state: FormState, formData: FormData) => {
   // 処理が成功したらルートへリダイレクトさせる
   redirect('/');
 };
+
+/**
+ * タスクを更新する非同期関数。
+ *
+ * @param id - 更新対象のタスクの ID 。
+ * @param state - FormState 型のオブジェクト。エラーメッセージを含む。
+ * @param formData - フォームから送信されるデータを含む FormData 型のオブジェクト。
+ * @returns エラーが発生した場合、FormState 型のオブジェクトを返す。
+ */
+export const updateTask = async (
+  id: string,
+  state: FormState,
+  formData: FormData,
+) => {
+  const newTask: Task = {
+    title: formData.get('title') as string,
+    description: formData.get('description') as string,
+    dueDate: formData.get('dueDate') as string,
+    isCompleted: Boolean(formData.get('isCompleted')),
+  };
+
+  // DB 接続とタスク作成時にエラーが発生した場合はエラーメッセージを state に設定して返却する
+  try {
+    await connectDb();
+    await TaskModel.updateOne({ _id: id }, updateTask);
+  } catch (error) {
+    state.error = 'タスクの更新に失敗しました';
+    return state;
+  }
+
+  // 処理が成功したらルートへリダイレクトさせる
+  redirect('/');
+};
--- src/components/EditTaskForm/EditTaskForm.tsx
+++ src/components/EditTaskForm/EditTaskForm.tsx
@@ -1,4 +1,18 @@
-const EditTaskForm = () => {
+'use client';
+
+import { TaskDocument } from '@/models/task';
+import React, { useState } from 'react';
+
+interface EditTaskFormProps {
+  task: TaskDocument;
+}
+
+const EditTaskForm: React.FC<EditTaskFormProps> = ({ task }) => {
+  const [title, setTitle] = useState(task.title);
+  const [description, setDescription] = useState(task.description);
+  const [dueDate, setDueDate] = useState(task.dueDate);
+  const [isCompleted, setIsCompleted] = useState(task.isCompleted);
+
   return (
     <div className="mx-auto mt-10 w-full max-w-sm">
       <form action="">
@@ -10,6 +24,8 @@ const EditTaskForm = () => {
             type="text"
             id="title"
             name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             required
             className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
           />
@@ -22,6 +38,8 @@ const EditTaskForm = () => {
             type="text"
             id="description"
             name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             required
             className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
           />
@@ -36,6 +54,8 @@ const EditTaskForm = () => {
             name="dueDate"
             min="2020-01-01"
             max="2999-12-31"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
             required
             className="mt-2 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300"
           />
@@ -45,6 +65,8 @@ const EditTaskForm = () => {
             type="checkbox"
             id="isCompleted"
             name="isCompleted"
+            checked={isCompleted}
+            onChange={(e) => setIsCompleted(e.target.checked)}
             className="mr-2 h-4 w-4"
           />
           <label htmlFor="isCompleted" className="text-sm">
