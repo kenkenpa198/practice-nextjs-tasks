b836269b364d38527bbcc6a5971a7bb39974892d
kenkenpa198 <kenkenpa198@gmail.com>
2024-07-15 19:31:02 +0900

    add getAllTasks in main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,27 @@
 import TaskCard from '@/components/TaskCard/TaskCard';
+import { TaskDocument } from '@/models/task';
 import Link from 'next/link';
 import { MdAddTask } from 'react-icons/md';
 
-export default function MainPage() {
+const getAllTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks`, {
+    cache: 'no-store',
+  });
+
+  // レスポンスで 200 を取得したらエラーをスローする
+  if (response.status !== 200) {
+    throw new Error();
+  }
+
+  const data = await response.json();
+  return data.tasks as TaskDocument[];
+};
+
+export default async function MainPage() {
+  // タスク一覧を取得する
+  const allTasks = await getAllTasks();
+  console.log(allTasks);
+
   return (
     <div className="pg-24 h-full overflow-y-auto p-8 text-gray-800">
       <header className="flex items-center justify-between">
