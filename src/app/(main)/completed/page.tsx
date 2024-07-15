e9a485cfd72024e7a7089b21c9be2ea05c767093
kenkenpa198 <kenkenpa198@gmail.com>
2024-07-15 20:31:43 +0900

    46. 完了タスクページの実装

--- src/app/(main)/completed/page.tsx
+++ src/app/(main)/completed/page.tsx
@@ -1,6 +1,22 @@
 import TaskCard from '@/components/TaskCard/TaskCard';
+import { TaskDocument } from '@/models/task';
 
-const CompletedTaskPage = () => {
+const getCompletedTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks/completed`, {
+    cache: 'no-store',
+  });
+
+  // レスポンスで 200 を取得したらエラーをスローする
+  if (response.status !== 200) {
+    throw new Error();
+  }
+
+  const data = await response.json();
+  return data.tasks as TaskDocument[];
+};
+
+const CompletedTaskPage = async () => {
+  const completedTasks = await getCompletedTasks();
   return (
     <div className="pg-24 h-full overflow-y-auto p-8 text-gray-800">
       <header className="flex items-center justify-between">
@@ -10,7 +26,9 @@ const CompletedTaskPage = () => {
       </header>
 
       <div className="mt-8 flex flex-wrap gap-4">
-        <TaskCard />
+        {completedTasks.map((task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
     </div>
   );
