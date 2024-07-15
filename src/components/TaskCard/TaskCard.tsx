94d8e8747fe052f44471af1a80409f9adb55bbf2
kenkenpa198 <kenkenpa198@gmail.com>
2024-07-15 19:59:11 +0900

    fix

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -22,8 +22,8 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
             {task.isCompleted ? 'Completed' : 'Incomplete'}
           </div>
           <div className="flex gap-4">
-            <TaskEditButton id={task.id} />
-            <TaskDeleteButton id={task.id} />
+            <TaskEditButton id={task._id} />
+            <TaskDeleteButton id={task._id} />
           </div>
         </div>
       </div>
